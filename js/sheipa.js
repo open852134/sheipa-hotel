b0a82304280d0eaad75d514d43f36bda5d0e06ef
open852134 <open852134@gmail.com>
2016-09-22 16:14:41 +0800

    更新room.html

--- js/sheipa.js
+++ js/sheipa.js
@@ -1,4 +1,23 @@
+/*
+*
+*
+*/
 
+// var filename = document.URL.replace(/^.*[\\\/]/, '');
+// if (filename != "index.html") {
+//     $('#mainNav').css("opacity", 1);
+//     $('#mainNav').css("top", 450);
+// }
+$(window).scroll(function() {
+
+
+        var heigh = $(window).scrollTop();
+        if (heigh > 900) {
+            $("#mainNav").css("opacity", 1).fadeTo('slow');
+        } else if (heigh < 900) {
+            $("#mainNav").css("opacity", 0).fadeTo('slow');
+        }
+    });
 
 // 輪播
 $(function() {
@@ -6,28 +25,20 @@ $(function() {
         $slider_txt = $('.slider_txt').find('.item'),
 
         showIndex = 0,
-        fadeOutSpeed = 2000, 
-        fadeInSpeed = 2000, 
-        defaultZ = 10, 
-        timer = 8000;           //輪播時間
-        imglength  = $slider_image.length;
+        fadeOutSpeed = 2000,
+        fadeInSpeed = 2000,
+        defaultZ = 10,
+        timer = 8000; //輪播時間
+    imglength = $slider_image.length;
     setInterval(function sequentialImg() {
         $slider_image.eq(showIndex).stop().fadeTo(fadeInSpeed, 1).css('zIndex', defaultZ).siblings('div').stop().fadeTo(fadeOutSpeed, 0).css('zIndex', defaultZ - 1);
         $slider_txt.eq(showIndex).stop().fadeTo(fadeInSpeed, 1).css('zIndex', defaultZ).siblings('div').stop().fadeTo(fadeOutSpeed, 0).css('zIndex', defaultZ - 1);
-        
+
         showIndex++;
         console.log(showIndex);
-        if (showIndex >= imglength) showIndex = 0;   
+        if (showIndex >= imglength) showIndex = 0;
     }, timer);
 });
 
 
 // Menu 選單
-$(window).scroll(function(){
-    var heigh = $(window).scrollTop();
-    if (heigh > 900) {
-        $("#mainNav").css("opacity",1).fadeTo('slow');
-    }else if (heigh < 900){
-        $("#mainNav").css("opacity",0).fadeTo('slow');
-    }
-});
